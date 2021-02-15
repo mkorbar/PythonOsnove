380a1a0c3ecd89da677c611134d0fee7b5fa3fc8
Matej Korbar <matej.korbar@gmail.com>
2021-02-15 16:41:58 +0100

    Save high score if better than last time, translate

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ while True:
     if guess == secret:
         print("You've guessed it - congratulations! It's number {0}. This was your {1} attempt."
               .format(secret, st_poizkusa))
+
+        with open('high_score.txt', 'r') as high_score_fp:
+            best_score = int(high_score_fp.read())
+
+        if st_poizkusa < best_score:
+            with open('high_score.txt', 'w') as high_score_fp:
+                high_score_fp.write(str(st_poizkusa))
+
         break
     elif guess > secret:
         print('Your attempt ({0}) is too high. This was your {1} attempt.'.format(guess, st_poizkusa))
