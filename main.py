e652a2ed2716f4f4c5251c600adde75b8727dc84
Matej Korbar <matej.korbar@gmail.com>
2021-02-18 16:43:33 +0100

    Save all the games' data

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+import json
 import random
+import datetime
 
 secret = random.randint(1, 10)
 st_poizkusa = 0
+
+name = input('Hi! What\'s your name?')
+
+with open('high_score.txt', 'r') as high_score_fp:
+    score_list = json.loads(high_score_fp.read())
+    print('Past results are:')
+    score_list.sort()
+    print(score_list)
 
 while True:
     st_poizkusa = st_poizkusa + 1
@@ -11,13 +21,15 @@ while True:
     if guess == secret:
         print("You've guessed it - congratulations! It's number {0}. This was your {1} attempt."
               .format(secret, st_poizkusa))
+        game_data = {
+            'time': str(datetime.datetime.now()),
+            'player name': name,
+            'score': st_poizkusa
+        }
+        score_list.append(game_data)
 
-        with open('high_score.txt', 'r') as high_score_fp:
-            best_score = int(high_score_fp.read())
-
-        if st_poizkusa < best_score:
-            with open('high_score.txt', 'w') as high_score_fp:
-                high_score_fp.write(str(st_poizkusa))
+        with open('high_score.txt', 'w') as high_score_fp:
+            high_score_fp.write(json.dumps(score_list, indent=4))
 
         break
     elif guess > secret:
